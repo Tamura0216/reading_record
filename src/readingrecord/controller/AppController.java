ed0709599a6b5546b943c6d07c3e84198cb2b08b
somat <somat@192.168.10.123>
2025-06-11 00:52:55 +0900

    scannerを閉じないよう修正、レイアウトの修正

--- src/readingrecord/controller/AppController.java
+++ src/readingrecord/controller/AppController.java
@@ -6,41 +6,36 @@ import readingrecord.service.BookManager;
 
 public class AppController {
 	public void runApp() {
-		Scanner scanner = new Scanner(System.in);
 		BookManager bookManager = new BookManager();
+		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("■読書記録アプリ■");
-		System.out.println("行いたい操作の数字を入力してください");
-		System.out.println("1：本の登録");
-		System.out.println("2：登録済みの本の情報を一覧表示");
-		System.out.println("3：登録済みの本のページを表示");
-		System.out.println("4：アプリの終了");
+		while (true) {
+			System.out.println("■読書記録アプリ■");
+			System.out.println("行いたい操作の数字を入力してください");
+			System.out.println("1：本の登録");
+			System.out.println("2：登録済みの本の情報を一覧表示");
+			System.out.println("3：登録済みの本のページを表示");
+			System.out.println("4：アプリの終了");
 
-		int input_num = scanner.nextInt();
+			int input_num = Integer.parseInt(scanner.nextLine());
 
-		switch (input_num) {
-		case 1:
-			System.out.println("登録したい本のタイトルを入力してください");
-			String bookTitle = scanner.nextLine();
-			System.out.println("登録したい本の著者を入力してください");
-			String author = scanner.nextLine();
-			System.out.println("登録したい本の著者を入力してください");
-			int page = scanner.nextInt();
-			bookManager.registerBook(bookTitle, author, page);
-			System.out.println("登録完了しました");
-			runApp();
-			break;
-		case 2:
-			bookManager.viewBooks();
-			runApp();
-			break;
-		case 3:
-			bookManager.totalPage();
-			runApp();
-			break;
-		case 4:
-			System.out.println("アプリを終了します");
-			break;
+			switch (input_num) {
+			case 1:
+				bookManager.registerBook();
+				break;
+			case 2:
+				bookManager.viewBooks();
+				break;
+			case 3:
+				bookManager.totalPage();
+				break;
+			case 4:
+				System.out.println("アプリを終了します");
+				return;
+			default:
+				System.out.println("不正な数字が入力されました");
+				break;
+			}
 		}
 	}
 }
--- src/readingrecord/service/BookManager.java
+++ src/readingrecord/service/BookManager.java
@@ -2,27 +2,41 @@ package readingrecord.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import readingrecord.model.Book;
 
 public class BookManager {
 	private List<Book> bookList = new ArrayList<>();
 	
-	public void registerBook(String bookTitle, String author, int page) {
+	public void registerBook() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("登録したい本のタイトルを入力してください");
+		String bookTitle = scanner.next();
+		System.out.println("登録したい本の著者を入力してください");
+		String author = scanner.next();
+		System.out.println("登録したい本のページ数を入力してください");
+		int page = Integer.parseInt(scanner.next());
 		Book book = new Book(bookTitle, author, page);
 		bookList.add(book);
+		System.out.println("登録完了しました");
+		System.out.println();
+		System.out.println();
 	}
 	
 	public void viewBooks(){
-		int i = 0;
+		int i = 1;
+		System.out.println("--登録されている本の一覧--");
 		for(Book book : bookList) {
-			System.out.println("--登録されている本の一覧--");
 			System.out.println(i);
 			System.out.println("タイトル：" + book.getBookTitle());
 			System.out.println("著者：" + book.getAuthor());
 			System.out.println("ページ数：" + book.getPage());
+			System.out.println();
 			i++;
 		}
+		System.out.println();
+		System.out.println();
 	}
 	
 	public void totalPage() {
@@ -31,5 +45,7 @@ public class BookManager {
 			total += book.getPage();
 		}
 		System.out.println("登録されている本の合計ページ数は" + total + "です");
+		System.out.println();
+		System.out.println();
 	}
 }
