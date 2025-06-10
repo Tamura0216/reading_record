0a78fa1d99a1b65ad8f51832bfab818f64c53738
somat <somat@192.168.10.123>
2025-06-11 00:22:24 +0900

    Mainクラスの作成

---
+++ src/readingrecord/Main.java
@@ -0,0 +1,12 @@
+package readingrecord;
+
+import readingrecord.controller.AppController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AppController appController = new AppController();
+		appController.runApp();
+	}
+
+}
